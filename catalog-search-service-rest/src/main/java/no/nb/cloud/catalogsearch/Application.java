8e5d1b15a966fe2d770c7c9242552242fb5068f6
Andreas Bjørnådal <bjornadal@gmail.com>
2015-05-21 12:22:35 +0200

    Initial commit

---
+++ catalog-search-service-rest/src/main/java/no/nb/cloud/catalogsearch/Application.java
@@ -0,0 +1,28 @@
+package no.nb.cloud.catalogsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
+@EnableConfigurationProperties
+@EnableEurekaClient
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableOAuth2Resource
+@RefreshScope
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ catalog-search-service-rest/src/main/java/no/nb/cloud/catalogsearch/service/package-info.java
@@ -0,0 +1,1 @@
+package no.nb.cloud.catalogsearch.service;
