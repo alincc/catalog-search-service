a9780717b7c5a923bd921a8036d2984296518deb
ronnymikalsen <ronny.mikalsen@nb.no>
2015-06-05 15:04:53 +0200

    Unit test for IndexServiceImpl

---
+++ catalog-search-service-rest/src/test/java/no/nb/microservices/catalogsearch/core/index/service/IndexServiceImplTest.java
@@ -0,0 +1,77 @@
+package no.nb.microservices.catalogsearch.core.index.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import no.nb.microservices.catalogsearch.core.index.model.SearchResult;
+import no.nb.microservices.catalogsearch.core.index.repository.IIndexRepository;
+import no.nb.microservices.catalogsearchindex.EmbeddedWrapper;
+import no.nb.microservices.catalogsearchindex.ItemResource;
+import no.nb.microservices.catalogsearchindex.SearchResource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.PagedResources.PageMetadata;
+
+import scala.annotation.meta.setter;
+
+@RunWith(MockitoJUnitRunner.class)
+public class IndexServiceImplTest {
+
+    private IndexServiceImpl indexService;
+    
+    @Mock
+    IIndexRepository indexRepository;
+    
+    @Before
+    public void setUp() {
+        indexService = new IndexServiceImpl(indexRepository);
+    }
+    
+    
+    @Test
+    public void whenBlaThenResult() {
+        
+        String query = "I love a good book";
+        
+        EmbeddedWrapper wrapper = new EmbeddedWrapper();
+        wrapper.getItems().add(createItem("id1"));
+        wrapper.getItems().add(createItem("id2"));
+        wrapper.getItems().add(createItem("id3"));
+
+        Pageable pageable = new PageRequest(0, 10);
+        PageMetadata page = new PageMetadata(wrapper.getItems().size(), 0, 3);
+        SearchResource searchResult = new SearchResource(page );
+        searchResult.setEmbedded(wrapper);
+        
+        
+        
+        when(indexRepository.search(query , pageable)).thenReturn(searchResult);
+        
+        SearchResult result = indexService.search(query, pageable);
+        
+        assertNotNull("Result should not be null", result);
+        assertEquals("Total elements should be 3", 3, result.getTotalElements());
+        assertEquals("Result should have 3 elements", 3, result.getIds().size());
+        
+    }
+
+
+    private ItemResource createItem(String id) {
+        ItemResource item = new ItemResource();
+        item.setItemId(id);
+        return item;
+    }
+
+}
