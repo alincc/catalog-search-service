701cae82a1d8c649480bbd4a06b021f6ba8714e8
ronnymikalsen <ronny.mikalsen@nb.no>
2015-06-08 15:16:03 +0200

    feign

--- catalog-search-service-rest/src/main/java/no/nb/microservices/catalogsearch/core/item/repository/IItemRepository.java
+++ catalog-search-service-rest/src/main/java/no/nb/microservices/catalogsearch/core/item/repository/IItemRepository.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "catalog-item-service")
+@FeignClient("catalog-item-service")
 public interface IItemRepository {
 
     @RequestMapping(method = RequestMethod.GET, value = "/item/{id}")
