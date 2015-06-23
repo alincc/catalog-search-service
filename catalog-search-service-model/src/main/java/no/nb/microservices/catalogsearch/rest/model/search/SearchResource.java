bee340aa308c3b7a19835dc63a2a8a6dca74e8ea
ronnymikalsen <ronny.mikalsen@nb.no>
2015-06-23 15:23:57 +0200

    Fixed duplicate embedded elements

--- catalog-search-service-model/src/main/java/no/nb/microservices/catalogsearch/rest/model/search/SearchResource.java
+++ catalog-search-service-model/src/main/java/no/nb/microservices/catalogsearch/rest/model/search/SearchResource.java
@@ -5,6 +5,7 @@ import org.springframework.hateoas.ResourceSupport;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -25,10 +26,12 @@ public class SearchResource extends ResourceSupport {
         this.metadata = metadata;
     }
 
+    @JsonIgnore
     public PageMetadata getMetadata() {
         return metadata;
     }
 
+    @JsonIgnore
     public EmbeddedWrapper getEmbedded() {
         return wrapper;
     }
