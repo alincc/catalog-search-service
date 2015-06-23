0d611dfd87b5f002a1f591e866c6c612e2534a4c
ronnymikalsen <ronny.mikalsen@nb.no>
2015-06-23 15:24:25 +0200

    Removed unused annotation

--- catalog-search-service-model/src/main/java/no/nb/microservices/catalogsearch/rest/model/search/SearchResource.java
+++ catalog-search-service-model/src/main/java/no/nb/microservices/catalogsearch/rest/model/search/SearchResource.java
@@ -3,14 +3,12 @@ package no.nb.microservices.catalogsearch.rest.model.search;
 import org.springframework.hateoas.PagedResources.PageMetadata;
 import org.springframework.hateoas.ResourceSupport;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown=true)
-@JsonAutoDetect(fieldVisibility=JsonAutoDetect.Visibility.ANY)
 public class SearchResource extends ResourceSupport {
     @JsonProperty("page")
     private PageMetadata metadata;
